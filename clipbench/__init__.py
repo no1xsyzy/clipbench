284cad6cfe9fcb1c048fcf1fd3be90e4325ee24e
no1xsyzy <mf20070535@126.com>
2021-04-19 01:55:23 +0800

    enhance(mimetypes): Now can choose mimetypes

--- clipbench/__init__.py
+++ clipbench/__init__.py
@@ -4,24 +4,34 @@ from threading import Lock
 
 from PyQt5 import QtWidgets
 from PyQt5.Qt import QApplication
-from PyQt5.QtCore import QSize
+from PyQt5.QtCore import QSize, QMimeData, QItemSelectionModel, QByteArray
 from PyQt5.QtWidgets import QMainWindow, QTextEdit, QGridLayout, QListWidget, QLineEdit, QHBoxLayout, QWidget
+
+from .hexdump import to_hex_dump, from_hex_dump
+
+BUFFER_TO_CLIPBOARD = 'BUFFER_TO_CLIPBOARD'
+CLIPBOARD_TO_BUFFER = 'CLIPBOARD_TO_BUFFER'
 
 
 class ClipboardWorkbench(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.transfer = Lock()
+        self._transfer = Lock()
+        self._sync_direction = ''
+        self._formats = []
+        self._current_format = None
+        self.format_data = {}
 
         self.setMinimumSize(QSize(440, 240))
         self.setWindowTitle("Clipboard Workbench")
 
         self.setCentralWidget(self.central_widget)
 
-        self.buffer.setPlainText(QApplication.clipboard().text())
+        self.clipboard_changed()
         QApplication.clipboard().dataChanged.connect(self.clipboard_changed)
         self.buffer.textChanged.connect(self.text_changed)
+        self.mime_select.currentRowChanged.connect(self.mime_select_changed)
 
     @cached_property
     def central_widget(self):
@@ -39,7 +49,6 @@ class ClipboardWorkbench(QMainWindow):
         grid_layout.addWidget(self.buffer, 1, 1)
         grid_layout.addWidget(self.command_line, 2, 1)
         grid_layout.addWidget(self.operation_select, 1, 2, 2, 1)
-        self.setLayout(grid_layout)
         return grid_layout
 
     @cached_property
@@ -48,7 +57,7 @@ class ClipboardWorkbench(QMainWindow):
         return buffer
 
     @cached_property
-    def mime_select(self):
+    def mime_select(self) -> QListWidget:
         mime_select = QListWidget()
         return mime_select
 
@@ -67,19 +76,92 @@ class ClipboardWorkbench(QMainWindow):
         clipboard_select = QHBoxLayout()
         return clipboard_select
 
+    @property
+    def formats(self):
+        return self._formats
+
+    @formats.setter
+    def formats(self, value):
+        self._formats = value
+        self.mime_select.clear()
+        self.mime_select.addItems(self.formats)
+        if not value:
+            self.current_format = None
+        elif self.current_format not in value:
+            for fmt in ['text/plain', 'text/html', 'application/x-qt-windows-mime;value="Rich Text Format"']:
+                if fmt in value:
+                    self.current_format = fmt
+                    break
+            else:
+                self.current_format = value[0]
+
+    @property
+    def current_format(self):
+        return self._current_format
+
+    @current_format.setter
+    def current_format(self, value):
+        self._current_format = value
+        if self.current_format is None:
+            self.mime_select.setCurrentRow(0, QItemSelectionModel.Deselect)
+        else:
+            self.mime_select.setCurrentRow(self.formats.index(self.current_format))
+
     def text_changed(self):
-        if self.transfer.locked():
+        if self._transfer.locked():
             return
-        with self.transfer:
-            text = self.buffer.toPlainText()
-            QApplication.clipboard().setText(text)
+        with self._transfer:
+            self._sync_direction = BUFFER_TO_CLIPBOARD
+            self.sync_buffer_to_clipboard()
+            self._sync_direction = None
+
+    def mime_select_changed(self, row):
+        if self._transfer.locked():
+            return
+        with self._transfer:
+            self._sync_direction = CLIPBOARD_TO_BUFFER
+            self._current_format = self.formats[row]
+            self.sync_clipboard_to_buffer()
+            self._sync_direction = None
 
     def clipboard_changed(self):
-        if self.transfer.locked() or QApplication.clipboard().ownsClipboard():
+        clipboard = QApplication.clipboard()
+        if self._transfer.locked() or clipboard.ownsClipboard():
             return
-        with self.transfer:
-            text = QApplication.clipboard().text()
-            self.buffer.setPlainText(text)
+        with self._transfer:
+            self._sync_direction = CLIPBOARD_TO_BUFFER
+            mime = clipboard.mimeData()
+            self.formats = mime.formats()
+            self.format_data = {fmt: mime.data(fmt) for fmt in self.formats}
+            self.sync_clipboard_to_buffer()
+            self._sync_direction = None
+
+    def sync_buffer_to_clipboard(self):
+        assert self._sync_direction == BUFFER_TO_CLIPBOARD
+        self.formats = [self.current_format]
+        self.format_data = {self.current_format: self.format_data[self.current_format]}
+        if self.current_format == 'text/plain':
+            data = self.buffer.toPlainText().encode('utf-8')
+        elif self.current_format == 'text/html':
+            data = self.buffer.toHtml().encode('utf-8')
+        else:
+            data = from_hex_dump(self.buffer.toPlainText())
+        self.format_data = {self.current_format: data}
+        mimedata = QMimeData()
+        mimedata.setData(self.current_format, data)
+        QApplication.clipboard().setMimeData(mimedata)
+
+    def sync_clipboard_to_buffer(self):
+        assert self._sync_direction == CLIPBOARD_TO_BUFFER
+        byte_data = self.format_data[self.current_format]
+        if isinstance(byte_data, QByteArray):
+            byte_data = byte_data.data()
+        if self.current_format == 'text/plain':
+            self.buffer.setPlainText(byte_data.decode('utf-8'))
+        elif self.current_format == 'text/html':
+            self.buffer.setHtml(byte_data.decode('utf-8'))
+        else:
+            self.buffer.setPlainText(to_hex_dump(byte_data))
 
 
 def main():
---
+++ clipbench/hexdump.py
@@ -0,0 +1,33 @@
+from math import ceil
+
+
+def to_hex_dump(b: bytes):
+    ss = []
+    lenn = ceil(len(b).bit_length() / 4 - 1)
+    n = 0
+    while b:
+        ss.append(
+            hex(n)[2:].rjust(lenn, "0") +
+            " | " +
+            " ".join(("0" + hex(c)[2:])[-2:] for c in b[:16]).ljust(48) +
+            "| " +
+            ''.join(byte_to_char(c) for c in b[:16])
+        )
+        b = b[16:]
+        n += 1
+    return "\n".join(ss)
+
+
+def byte_to_char(c: int):
+    try:
+        c = bytes([c]).decode('ascii')
+        if c.isprintable():
+            return c
+        else:
+            return '.'
+    except UnicodeDecodeError:
+        return '.'
+
+
+def from_hex_dump(ss: str):
+    return bytes([int(c, 16) for s in ss.split("\n") for c in s.split("|")[1].strip().split(" ")])
